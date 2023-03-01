687c4e7dc667da74dd9d95247f6697531438a27b
Valentin ISHIMWE <ishimwevalentin3@gmail.com>
2023-03-01 23:02:36 +0200

    Added Eslint config to provide hints / enforce coding styles

--- src/db.js
+++ src/db.js
@@ -1,27 +1,25 @@
-import { Sequelize } from "sequelize"
+import { Sequelize } from 'sequelize';
 
-
-const dbUser = process.env.POSTGRES_USER
-const dbPassword = process.env.POSTGRES_PASSWORD
-const dbName = process.env.POSTGRES_DB
-const dbHost = process.env.POSTGRES_HOST
-const dbPort = process.env.POSTGRES_PORT
-
+// const dbUser = process.env.POSTGRES_USER
+// const dbPassword = process.env.POSTGRES_PASSWORD
+// const dbName = process.env.POSTGRES_DB
+// const dbHost = process.env.POSTGRES_HOST
+// const dbPort = process.env.POSTGRES_PORT
 
 // const sequelize = new Sequelize(`postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`)
 
-// Using this connection - the above env variables were not working
-const sequelize = new Sequelize(`postgres://admin:admin123@database:5432/ecommerce`)
+// Using this connection - the above env variables were not working as expected
+const sequelize = new Sequelize(
+  'postgres://admin:admin123@database:5432/ecommerce'
+);
 
 export const connectDB = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
+  try {
+    await sequelize.authenticate();
+    console.log('SUCCESS: Connection has been established successfully.');
+  } catch (error) {
+    console.error('error Unable to connect to the database:', error);
+  }
+};
 
-connectDB()
-
-export default sequelize
+export default sequelize;
