a371c571b5280f24e92a4b204a0c86241ee7f861
Valentin ISHIMWE <ishimwevalentin3@gmail.com>
2023-03-02 11:12:19 +0200

    Fixing problems raised by hound bot on pull request

--- src/app.js
+++ src/app.js
@@ -1,31 +1,30 @@
-import express from "express";
-import env from "dotenv"
-import cors from "cors";
-import morgan from "morgan";
-import allroutes from "./routes/index"
+import express from 'express';
+import env from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+import allroutes from './routes/index';
 
-env.config()
+env.config();
 
 // Testing if the db connection works when running from docker container
 // Feel free to comment it, if not using docker yet
-import { connectDB } from "./db";
-connectDB()
+import { connectDB } from './db';
+connectDB();
 
+const app = express();
 
-const app = express()
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
 
-app.use(express.json())
-app.use(cors())
-app.use(morgan("dev"))
-
-app.use(allroutes)
+app.use(allroutes);
 app.use((req, res) => {
-  res.status(404).json({ message: 'Page not found' })
-})
+    res.status(404).json({ message: 'Page not found' });
+});
 
-const port = process.env.PORT
+const port = process.env.PORT;
 app.listen(port, () => {
-  console.log('server started,', port)
-})
+    console.log('server started,', port);
+});
 
-export default app
+export default app;
--- src/db.js
+++ src/db.js
@@ -1,17 +1,17 @@
-import { Sequelize } from "sequelize"
+import { Sequelize } from 'sequelize';
 
-
-const dbUser = process.env.POSTGRES_USER
-const dbPassword = process.env.POSTGRES_PASSWORD
-const dbName = process.env.POSTGRES_DB
-const dbHost = process.env.POSTGRES_HOST
-const dbPort = process.env.POSTGRES_PORT
-
+const dbUser = process.env.POSTGRES_USER;
+const dbPassword = process.env.POSTGRES_PASSWORD;
+const dbName = process.env.POSTGRES_DB;
+const dbHost = process.env.POSTGRES_HOST;
+const dbPort = process.env.POSTGRES_PORT;
 
 // const sequelize = new Sequelize(`postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`)
 
 // Using this connection - the above env variables were not working
-const sequelize = new Sequelize(`postgres://admin:admin123@database:5432/ecommerce`)
+const sequelize = new Sequelize(
+    `postgres://admin:admin123@database:5432/ecommerce`
+);
 
 export const connectDB = async () => {
     try {
@@ -20,8 +20,7 @@ export const connectDB = async () => {
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
-}
+};
 
-connectDB()
 
-export default sequelize
+export default sequelize;
