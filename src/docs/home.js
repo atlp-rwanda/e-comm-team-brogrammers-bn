435e8fcb923323fe348f1d44aef48e981e5628b1
blackd44 <irabd44@gmail.com>
2023-03-07 14:04:31 +0200

    Chore(swagger): swagger documentation setup #184581324

---
+++ src/docs/home.js
@@ -0,0 +1,13 @@
+/**
+ * @swagger
+ * /home:
+ *    get:
+ *       tags:
+ *         - Home
+ *       description: Get welcome message
+ *       summary: Get welcome message
+ *       operationId: welcome
+ *       responses:
+ *         '200':
+ *           description: successful operation
+ */
