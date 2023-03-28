f3b46fec497502d28c40c43d2462d2a5769350db
marita <marthairadukunda1@gmail.com>
2023-03-28 21:38:05 +0300

    feat(buyers CRUD orders):buyers should be able to CRUD the orders #184797809

--- src/controllers/checkout.js
+++ src/controllers/checkout.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /* eslint-disable import/named */
 import {
   users, order, orderitem, products, carts
@@ -60,4 +61,52 @@ export const createOrder = async (req, res) => {
   await user.cart.save();
 
   res.json({ message: 'Order was created successfully', order: userOrder });
+};
+
+export const updateOrder = async (req, res) => {
+  try {
+    const { order_id } = req.params;
+    const updatedOrder = req.body;
+    const orders = await order.findOne({
+      where: { id: order_id }
+    });
+
+    if (!orders) {
+      return res.status(404).json({ error: 'Order not found.' });
+    }
+    // Ensure that the buyer is authenticated and authorized to update the order
+    if (orders.buyerId !== req.user.id) {
+      return res.status(403).json({ error: 'You are not authorized to update this order.' });
+    }
+
+    // Update the order with the new data
+    await orders.update(updatedOrder);
+
+    return res.status(200).json(orders);
+  } catch (error) {
+    return res.status(500).json({ error: 'Server error.' });
+  }
+};
+
+export const deleteOrder = async (req, res) => {
+  try {
+    const { order_id } = req.params;
+    const orders = await order.findOne({
+      where: { id: order_id }
+    });
+    if (!orders) {
+      return res.status(404).json({ error: 'Order not found.' });
+    }
+    // Ensure that the buyer is authenticated and authorized to update the order
+    if (orders.buyerId !== req.user.id) {
+      return res.status(403).json({ error: 'You are not authorized to delete this order.' });
+    }
+
+    // Delete the order from the database
+    await orders.destroy();
+
+    return res.status(201).json({ message: 'Order deleted successfully!' });
+  } catch (error) {
+    return res.status(500).json({ error: 'Server error.' });
+  }
 };
